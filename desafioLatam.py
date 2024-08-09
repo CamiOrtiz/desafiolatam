ba0997fd2d8787ca453492957d2a78592efa634f
CamiOrtiz <101368304+CamiOrtiz@users.noreply.github.com>
2024-08-09 13:43:21 -0400

    Update desafioLatam.py

--- desafioLatam.py
+++ desafioLatam.py
@@ -1,1 +1,5 @@
+//Se ha diseñado un programa en Python con la intención de imprimir un mensaje de bienvenida a los usuarios cuyo nombre coincide con "Alex". Sin embargo, tras realizar algunas modificaciones en el código, el programa ha dejado de funcionar correctamente y ya no muestra el mensaje de bienvenida adecuado. Corrige el error en el siguiente código para que funcione correctamente.//
 
+nombreUsuario = "Juan" # Modifica esta línea
+if nombreUsuario == "Alex":
+  print("Bienvenido, Alex");
