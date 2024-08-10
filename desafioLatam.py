19903432099fcffdee45e395fa273dfd6120b813
CamiOrtiz <camila.ortiz.sia@gmail.com>
2024-08-09 22:43:06 -0400

    primeros ejercicios

--- desafioLatam.py
+++ desafioLatam.py
@@ -1,5 +1,16 @@
-//Se ha diseñado un programa en Python con la intención de imprimir un mensaje de bienvenida a los usuarios cuyo nombre coincide con "Alex". Sin embargo, tras realizar algunas modificaciones en el código, el programa ha dejado de funcionar correctamente y ya no muestra el mensaje de bienvenida adecuado. Corrige el error en el siguiente código para que funcione correctamente.//
+#Se ha diseñado un programa en Python con la intención de imprimir un mensaje de bienvenida a los usuarios cuyo nombre coincide con "Alex". Sin embargo, tras realizar algunas modificaciones en el código, el programa ha dejado de funcionar correctamente y ya no muestra el mensaje de bienvenida adecuado. Corrige el error en el siguiente código para que funcione correctamente
 
 nombreUsuario = "Juan" # Modifica esta línea
 if nombreUsuario == "Alex":
-  print("Bienvenido, Alex");
+  print("Bienvenido, Alex")
+
+
+#Se tiene definida la función despedir, pero no se está llamando. Llama a la función despedir para que imprima "Adiós Mundo" en la consola.
+
+#def despedir():
+  #print("Adiós Mundo")
+
+def despedir():
+  print("Adiós Mundo")
+
+despedir()
